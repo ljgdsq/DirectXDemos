de772fdc9561adfd46b10d34578bc7d0dc03a3ef
zsl <zhengshulin@libii.com>
2020-10-23 17:17:39 +0800

    merge matser to main

---
+++ DirectXDemos/main.cpp
@@ -0,0 +1,103 @@
+#include <Windows.h>
+#include <string>
+#include <sstream>
+
+LRESULT  CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch (msg)
+    {
+    case WM_CLOSE:
+        PostQuitMessage(69);
+        break;
+
+    case WM_KEYDOWN:
+    {
+        if (wParam == 'T')
+        {
+            SetWindowText(hWnd, "KeyDown");
+        }
+    }break;
+
+    case WM_KEYUP:
+    {
+        if (wParam == 'T')
+        {
+            SetWindowText(hWnd, "KeyRelease");
+        }
+    }
+
+    case WM_CHAR:
+    {
+        static std::string title;
+        title.push_back(wParam);
+        SetWindowText(hWnd, title.c_str());
+    }break;
+
+    case WM_LBUTTONDOWN:
+    {
+        const POINTS pt = MAKEPOINTS(lParam);
+        std::ostringstream os;
+        os << "(" << pt.x << "," << pt.y << ")";
+        SetWindowText(hWnd, os.str().c_str());
+
+
+    }break;
+
+    default:
+        break;
+    }
+
+    return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+int CALLBACK WinMain(
+    HINSTANCE hInstance,
+    HINSTANCE hPreInstance,
+    LPSTR lpCmdLine,
+    int nCmdShow
+)
+{
+
+    const auto pClassName = "dx";
+    WNDCLASSEX wc = { 0 };
+    wc.cbSize = sizeof(wc);
+    wc.style = CS_OWNDC;
+    wc.lpfnWndProc = WndProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = hInstance;
+    wc.hIcon = nullptr;
+    wc.hCursor = nullptr;
+    wc.hbrBackground = nullptr;
+    wc.lpszMenuName = nullptr;
+    wc.lpszClassName = pClassName;
+    wc.hIconSm = nullptr;
+
+    RegisterClassEx(&wc);
+
+    HWND hWnd = CreateWindowEx(0,
+        pClassName, "DirectX", WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+        200, 200, 640, 480, nullptr, nullptr, hInstance,nullptr);
+
+
+    ShowWindow(hWnd, SW_SHOW);
+
+    MSG msg;
+
+    BOOL gResult;
+
+    while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    if (gResult != -1)
+    {
+        return msg.wParam;
+    }
+
+
+
+    return -1;
+}
