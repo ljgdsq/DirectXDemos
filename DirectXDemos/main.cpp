13a7fb039951ce9601290290850b2072d8aa63ff
zsl <zhengshulin@libii.com>
2020-10-24 15:18:06 +0800

    add message event

--- DirectXDemos/main.cpp
+++ DirectXDemos/main.cpp
@@ -1,5 +1,20 @@
 #include <Windows.h>
 
+
+LRESULT  CALLBACK WndProc(HWND hWnd,UINT msg,WPARAM wParam,LPARAM lParam)
+{
+    switch (msg)
+    {
+    case WM_CLOSE:
+        PostQuitMessage(69);
+        break;
+    default:
+        break;
+    }
+
+    return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
 int CALLBACK WinMain(
     HINSTANCE hInstance,
     HINSTANCE hPreInstance,
@@ -12,7 +27,7 @@ int CALLBACK WinMain(
     WNDCLASSEX wc = { 0 };
     wc.cbSize = sizeof(wc);
     wc.style = CS_OWNDC;
-    wc.lpfnWndProc = DefWindowProc;
+    wc.lpfnWndProc = WndProc;
     wc.cbClsExtra = 0;
     wc.cbWndExtra = 0;
     wc.hInstance = hInstance;
@@ -31,10 +46,23 @@ int CALLBACK WinMain(
 
 
     ShowWindow(hWnd, SW_SHOW);
-    while (true)
-    {
 
+    MSG msg;
+
+    BOOL gResult;
+
+    while ((gResult=GetMessage(&msg,nullptr,0,0))>0)
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
     }
 
-    return 0;
+    if (gResult!=-1)
+    {
+        return msg.wParam;
+    }
+    
+
+
+    return -1;
 }
