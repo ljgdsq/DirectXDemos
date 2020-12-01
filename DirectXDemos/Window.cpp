bbdd95a20a8837a523ba55ff9cd7bd53fa4b3305
zsl <zhengshulin@libii.com>
2020-12-01 10:24:08 +0800

    设置icon

---
+++ DirectXDemos/Window.cpp
@@ -0,0 +1,103 @@
+#include "Window.h"
+#ifdef _DEBUG
+#include "Utils/WindowsMessageMap.h"
+#endif // DEBUG
+
+#include "resource.h"
+
+Window::WindowClass Window::WindowClass::wndClass;
+
+const char* Window::WindowClass::GetName() noexcept
+{
+    return wndClassName;
+}
+
+HINSTANCE Window::WindowClass::GetInstance() noexcept
+{
+    return wndClass.hInst;
+}
+
+Window::WindowClass::WindowClass()noexcept :hInst(GetModuleHandle(nullptr))
+{
+
+    WNDCLASSEX wc = { 0 };
+    wc.cbSize = sizeof(wc);
+    wc.style = CS_OWNDC;
+    wc.lpfnWndProc = HandleMsgSetup;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = GetInstance();
+    wc.hIcon =static_cast<HICON>(LoadImage(GetInstance(), MAKEINTRESOURCE(IDI_ICON2),IMAGE_ICON,32,32,0));
+    wc.hCursor = nullptr;
+    wc.hbrBackground = nullptr;
+    wc.lpszMenuName = nullptr;
+    wc.lpszClassName = GetName();
+    wc.hIconSm = static_cast<HICON>(LoadImage(GetInstance(), MAKEINTRESOURCE(IDI_ICON2), IMAGE_ICON, 16, 16, 0));
+
+    RegisterClassEx(&wc);
+}
+
+Window::WindowClass::~WindowClass()
+{
+    UnregisterClass(GetName(), GetInstance());
+}
+
+Window::Window(int width, int height, const char* name) noexcept
+{
+    RECT wr;
+    wr.left = 100;
+    wr.right = width + wr.left;
+    wr.top = 100;
+    wr.bottom = height + wr.top;
+
+    AdjustWindowRect(&wr, WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU, false);
+    hWnd = CreateWindow(WindowClass::GetName(),name, WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,CW_USEDEFAULT, CW_USEDEFAULT
+        ,wr.right - wr.left, wr.bottom - wr.top,nullptr,nullptr, WindowClass::GetInstance(), this);
+
+    ShowWindow(hWnd, SW_SHOWDEFAULT);
+
+}
+
+Window::~Window()
+{
+    DestroyWindow(hWnd);
+}
+
+LRESULT Window::HandleMsgSetup(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept
+{
+
+    if (msg==WM_NCCREATE)
+    {
+        const CREATESTRUCTW* const pCreate = reinterpret_cast<CREATESTRUCTW*>(lParam);
+        Window* const pWnd = static_cast<Window*>(pCreate->lpCreateParams);
+        SetWindowLongPtr(hWnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(pWnd));
+        SetWindowLongPtr(hWnd, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(&Window::HandleMsgThunk));
+        return pWnd->HandleMsg(hWnd, msg, wParam, lParam);
+    }
+
+    return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+LRESULT Window::HandleMsgThunk(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept
+{
+    Window* const pWnd = reinterpret_cast<Window*>(GetWindowLongPtr(hWnd, GWLP_USERDATA));
+    return pWnd->HandleMsg(hWnd, msg, wParam, lParam);
+}
+
+LRESULT Window::HandleMsg(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept
+{
+#ifdef _DEBUG
+    static WindowsMessageMap mm;
+    OutputDebugString(mm(msg, lParam, wParam).c_str());
+#endif // DEBUG
+
+
+    switch (msg)
+    {
+    case WM_CLOSE:
+        PostQuitMessage(0);
+        return 0;
+    }
+    return DefWindowProc(hWnd, msg, wParam, lParam);
+
+}
--- DirectXDemos/main.cpp
+++ DirectXDemos/main.cpp
@@ -2,55 +2,9 @@
 #include <string>
 #include <sstream>
 
+#include "Window.h"
 #include "Utils/WindowsMessageMap.h"
 
-LRESULT  CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-
-    static WindowsMessageMap messgaeMap;
-
-    OutputDebugString(messgaeMap(msg,lParam,wParam).c_str());
-    switch (msg)
-    {
-    case WM_CLOSE:
-        PostQuitMessage(69);
-        break;
-
-    case WM_KEYDOWN:
-    {
-        /*     static std::string title;
-             title.push_back(wParam);
-             SetWindowText(hWnd, title.c_str());*/
-    }break;
-
-    case WM_KEYUP:
-    {
-      
-    }
-
-    case WM_CHAR:
-    {
-         static std::string title;
-         title.push_back(wParam);
-         SetWindowText(hWnd, title.c_str());
-    }break;
-
-    case WM_LBUTTONDOWN:
-    {
-        const POINTS pt = MAKEPOINTS(lParam);
-        std::ostringstream os;
-        os << "(" << pt.x << "," << pt.y << ")";
-        SetWindowText(hWnd, os.str().c_str());
-
-
-    }break;
-
-    default:
-        break;
-    }
-
-    return DefWindowProc(hWnd, msg, wParam, lParam);
-}
 
 int CALLBACK WinMain(
     HINSTANCE hInstance,
@@ -59,30 +13,8 @@ int CALLBACK WinMain(
     int nCmdShow
 )
 {
-
-    const auto pClassName = "dx";
-    WNDCLASSEX wc = { 0 };
-    wc.cbSize = sizeof(wc);
-    wc.style = CS_OWNDC;
-    wc.lpfnWndProc = WndProc;
-    wc.cbClsExtra = 0;
-    wc.cbWndExtra = 0;
-    wc.hInstance = hInstance;
-    wc.hIcon = nullptr;
-    wc.hCursor = nullptr;
-    wc.hbrBackground = nullptr;
-    wc.lpszMenuName = nullptr;
-    wc.lpszClassName = pClassName;
-    wc.hIconSm = nullptr;
-
-    RegisterClassEx(&wc);
-
-    HWND hWnd = CreateWindowEx(0,
-        pClassName, "DirectX", WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
-        200, 200, 640, 480, nullptr, nullptr, hInstance, nullptr);
-
-
-    ShowWindow(hWnd, SW_SHOW);
+    Window wind(300,200,"dx");
+   
 
     MSG msg;
 
